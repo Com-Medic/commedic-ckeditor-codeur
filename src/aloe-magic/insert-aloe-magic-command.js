5c5f1d41d74a5de6fb7ac3346fb521a9bafda2a6
Thomas Ferary <thomas.ferary@com-medic.com>
2023-01-09 15:55:01 +0100

    Fix: phrase without end now detected

--- src/aloe-magic/insert-aloe-magic-command.js
+++ src/aloe-magic/insert-aloe-magic-command.js
@@ -13,19 +13,114 @@ export default class InsertAloeMagicCommand extends Command {
 		const selection = this.editor.model.document.selection;
 		const range = selection.getFirstRange();
 		let textInput = '';
+		let textArray = Array();
 		for ( const item of range.getItems() ) {
 			if ( item.is( 'textProxy' )) {
 				if(!item.data.includes(this.cardFilterTitle) && !item.data.includes(this.lineFilterTitle) && !item.data.includes(this.textFilterTitle)){
 					textInput += item.data + ' ';
+					textArray.push(item.data);
 				}
 			}
 		}
-		textInput = textInput.slice( 0, -1 );
+		for await (let textArrayItem of textArray){
+			// textArrayItem = textArrayItem.slice( 0, -1 );
+			textArrayItem = textArrayItem.replace( /[\n\r]/g, ' ' );
+			textArrayItem = textArrayItem.replace( '’', '\'' );
+			let splitTextArrayItem = textArrayItem.match(/[^\.!\?]+[\.!\?]+/g);
+			if(splitTextArrayItem === null){
+				splitTextArrayItem = Array();
+				splitTextArrayItem[0] = textArrayItem;
+			}
+			splitTextArrayItem.forEach( item => {
+				textArrayItem = textArrayItem.replace(item,'');
+			})
+			splitTextArrayItem[splitTextArrayItem.length] = textArrayItem;
+			for await (let text of splitTextArrayItem){
+				if(text !== ''){
+					await axios.get(params.endpoint, {
+						params: { text },
+						headers: {'Content-Type': 'application/json'}
+					}).then((response) => {
+						this.editor.model.change(writer => {
+							let aloeMagic = "";
+							const filters = JSON.parse(localStorage.getItem('filters'));
+							const filtersJson = window.btoa(unescape(encodeURIComponent(JSON.stringify(filters))));
+							const id = uuidv4();
+							const data = JSON.stringify(response.data);
+							const dataJson = window.btoa(unescape(encodeURIComponent(data)));
+							aloeMagic = writer.createElement('aloeMagic', {
+								id: id,
+								'data-json': dataJson,
+								'data-filters': filtersJson,
+								contenteditable: false
+							});
+							const textView = writer.createElement('paragraph')
+							writer.appendText(text, textView);
+							writer.append(textView, aloeMagic);
+
+							let p;
+							if(filters.card.display){
+								p = writer.createElement('paragraph')
+								writer.appendText(this.cardFilterTitle, p);
+
+								if(filters.card.vowel){
+									writer.appendText(' | Voyelles', p);
+								}
+								if(filters.card.consonant){
+									writer.appendText(' | Consonnes', p);
+								}
+
+								writer.append(p, aloeMagic);
+							}
+
+
+							if(filters.line.display){
+								p = writer.createElement('paragraph')
+								writer.appendText(this.lineFilterTitle, p);
+
+								if(filters.line.vowel){
+									writer.appendText(' | Voyelles', p);
+								}
+								if(filters.line.consonant){
+									writer.appendText(' | Consonnes', p);
+								}
+
+								writer.append(p, aloeMagic);
+							}
+
+							if(filters.text.display){
+								p = writer.createElement('paragraph')
+								writer.appendText(this.textFilterTitle, p);
+
+								if(filters.text.color){
+									writer.appendText(' | Couleurs', p);
+								}
+								if(filters.text.script){
+									writer.appendText(' | Script', p);
+								}
+								if(!filters.text.script){
+									writer.appendText(' | Cursif', p);
+								}
+
+								writer.append(p, aloeMagic);
+							}
+							this.editor.model.insertContent(aloeMagic,  this.editor.model.document.selection.getFirstPosition() );
+						});
+					}).catch(err => {
+						console.error(err);
+					});
+				}
+			}
+
+		}
+
+		/*textInput = textInput.slice( 0, -1 );
 		textInput = textInput.replace( /[\n\r]/g, ' ' );
 		textInput = textInput.replace( '’', '\'' );
-		let splitText = textInput.match(/[^\.!\?]+[\.!\?]+/g);
+		let splitText = textInput.match(/[^\.!\?]+[\.!\?]+/g);*/
+
 		// splitText = splitText.reverse();
-		for await (let text of splitText){
+		/*for await (let text of splitText){
 			await axios.get(params.endpoint, {
 				params: { text },
 				headers: {'Content-Type': 'application/json'}
@@ -98,7 +193,8 @@ export default class InsertAloeMagicCommand extends Command {
 			}).catch(err => {
 				console.error(err);
 			});
-		}
+		}*/
+
 		/*if(textInput !== '') {
 			axios.get(params.endpoint, {
 				params: { text: textInput },
